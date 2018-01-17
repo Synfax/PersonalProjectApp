356607cbdec97e080779f682ce7ae5b8f7e0bffc
Paul Spasojevic <paulspas27@gmail.com>
2018-01-17 19:15:09 +0800

    Commit

--- app/src/main/java/com/example/paul/project/SettingsActivity.java
+++ app/src/main/java/com/example/paul/project/SettingsActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -132,6 +133,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         if (actionBar != null) {
             // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
 
         }
     }
@@ -260,8 +262,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
+
             }
             return super.onOptionsItemSelected(item);
         }
